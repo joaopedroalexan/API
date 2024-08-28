1e263fdc79d5380b1044cf5fc2072089f61a5788
joaopedroalexan <joaopedroalexsilva@gmail.com>
2024-08-28 13:58:58 -0300

    API Cadastro

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express') //importa o módulo express
+const { use } = require('express/lib/application')
 
 //Define uma class para organizar a lógica da aplicação
 
@@ -17,10 +18,56 @@ class AppControllers{
     }
     //Define as rotas da nossa API
     routes(){
+        const users = []
+
+        this.express.post("/users", (req,res)=>{
+            const {id, nome, email, senha} = req.body
+            users.push({id,nome,email,senha})
+            console.log(users);
+            res.status(200).send({menssage:"Usuário cadastrado com sucesso"})
+        })  
+
+        this.express.post("/auth", (req,res)=>{
+            const {email, senha} = req.body
+
+            const user_email = users.find((user_email) => user_email.email == email)
+            console.log(user_email)
+            if(user_email){
+
+                const user_senha = users.find((user_senha) => user_senha.senha == senha)
+                console.log(user_senha)
+                if(user_senha){
+                    res.status(200).send({message:"Usuário logado!"})
+                } 
+                else {
+                    res.status(400).send({menssage:"Usuário não encontrado"})
+                }
+            } 
+            else {
+                res.status(400).send({menssage:"Usuário não encontrado"})
+            }
+
+            
+        })  
+        
+        
+        this.express.get("/users/:id", (req,res)=>{
+            const{id} = req.params;
+            const user = users.find((user) => user.id == id)
+
+            if(user){
+                res.status(200).send(user)
+            } else {
+                res.status(400).send({menssage:"Usuário não encontrado"})
+            }
+        })  
+
+
+
         //Define uma rota Get para o caminho health
         this.express.get("/health/", (req, res)=>{
             res.send({
-                nome: "João Pedro Alexandre",
+                nome: "Joao pedro alexandre",
                 idade: "17 anos",
                 CPF: "43604104840"
              })
